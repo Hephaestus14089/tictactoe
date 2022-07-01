a4665c7c356e395bbb9212c22759fbf0fdc2a589
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-07-01 18:27:05 +0530

    redesign square
    wierd ui issue in squares solved using vertical-align css property in the styling of squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,23 +1,28 @@
 const style = {
+  'display': "inline-block",
+  'vertical-align': "middle",
   'height': "100px",
   'width': "100px",
   'background-color': "#fff",
   'border': "3px solid",
   'border-color': "#000",
   'margin': "0",
-  'padding': "0"
+  'padding': "0",
+  'font-size': "80px",
+  'color': "#000",
+  'text-align': "center"
 };
 
 const Square = (props) => {
 
   return (
-    <button
+    <p
       className="square"
       style={style}
       onClick={() => { if (props.occupant === " ") props.onClick(); }}
     >
       {props.occupant}
-    </button>
+    </p>
   );
 };
 
