1466d3c151e0349015c71238173112732a7c5b98
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-07-02 17:52:27 +0530

    create Trays.js
    skeleton code of tray components added in Trays.js, TODO added in each component

---
+++ src/components/Trays.js
@@ -0,0 +1,21 @@
+const UpperTray = () => {
+  /*
+   * TODO:
+   * - dark/light mode
+   */
+};
+
+const MidRightTray = () => {
+  /*
+   * TODO:
+   * - no. of moves: total, x, y
+   * Next Player / Winner / Draw
+   */
+};
+
+const LowerTray = () => {
+  /*
+   * TODO:
+   * - new match
+   */
+};
